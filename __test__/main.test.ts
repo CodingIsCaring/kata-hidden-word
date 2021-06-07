6f2a76d0af7193dacd9dc8b9b448a8dc3e3025c1
yodralopez <yodralopez@gmail.com>
2021-06-07 17:31:43 +0100

    RED decode ble word when receives 124

--- __test__/main.test.ts
+++ __test__/main.test.ts
@@ -1,10 +1,15 @@
 import { decodeWord } from '../src/main';
 
 describe('decodeWord should', () => {
-  it('decode a when receives 6', () => {
+  it('decode a word when receives 6', () => {
     expect(decodeWord(6)).toBe('a');
   });
-  it('no', () => {
+
+  it('decode no word when receives 80', () => {
     expect(decodeWord(80)).toBe('no');
+  });
+
+  it('decode ble word when receives 124', () => {
+    expect(decodeWord(124)).toBe('ble');
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
 export function decodeWord(encryptedWord: number): string {
+  if (encryptedWord === 80) {
+    return 'no';
+  }
   return 'a';
 }
