df49bba9564ee8df92de8768e2ff6e5f441c2bb2
Maria Soria <maria.it3@gmail.com>
2021-06-07 17:26:32 +0100

    First test

--- __test__/main.test.ts
+++ __test__/main.test.ts
@@ -1,5 +1,10 @@
-describe('Default test', () => {
-  it('should work', () => {
-    expect(2 + 2).toBe(4);
+import { decodeWord } from '../src/main';
+
+describe('decodeWord should', () => {
+  it('decode a when receives 6', () => {
+    expect(decodeWord(6)).toBe('a');
+  });
+  it('no', () => {
+    expect(decodeWord(80)).toBe('no');
   });
 });
---
+++ src/main.ts
@@ -0,0 +1,3 @@
+export function decodeWord(encryptedWord: number): string {
+  return 'a';
+}
