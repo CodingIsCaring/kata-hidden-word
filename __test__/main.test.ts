292e2cbf0a65d2472f7d3513892644342e247463
yodralopez <yodralopez@gmail.com>
2021-06-07 18:01:44 +0100

    all tests pass

--- __test__/main.test.ts
+++ __test__/main.test.ts
@@ -16,4 +16,12 @@ describe('decodeWord should', () => {
   it('decode lead word when receives 2467', () => {
     expect(decodeWord(2467)).toBe('lead');
   });
+
+  it('decode code word when receives 9074', () => {
+    expect(decodeWord(9074)).toBe('code');
+  });
+
+  it('decode leanmind word when receives 24685387', () => {
+    expect(decodeWord(24685387)).toBe('leanmind');
+  });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 export function decodeWord(encryptedWord: number): string {
-  if (encryptedWord === 80) {
-    return 'no';
-  } else if (encryptedWord === 124) {
-    return 'ble';
+  const encryptedWordString = encryptedWord.toString();
+
+  let decryptedWord = '';
+  for (let i = 0; i < encryptedWordString.length; i++) {
+    decryptedWord += decodeLetter(encryptedWordString[i]);
   }
-  return 'a';
+  return decryptedWord;
 }
+
+const decodeLetter = (letter: string): string => {
+  switch (letter) {
+    case '1':
+      return 'b';
+    case '2':
+      return 'l';
+    case '3':
+      return 'i';
+    case '4':
+      return 'e';
+    case '5':
+      return 'm';
+    case '6':
+      return 'a';
+    case '7':
+      return 'd';
+    case '8':
+      return 'n';
+    case '9':
+      return 'c';
+    default:
+      return 'o';
+  }
+  return '';
+};
